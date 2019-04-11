da68604f3758d73f4c6be743be30814ee0d9471d
beccadilts <46534697+beccadilts@users.noreply.github.com>
2019-04-11 17:31:15 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -27,6 +27,21 @@ var x = setInterval(function() {
   }
 }, 1000);
 
+function toggleNav(toggleClass, activeClass) {
+  if (document.querySelector('.' + toggleClass)) {
+    function toggle() {
+      if (!document.body.classList.contains(activeClass)) {
+        document.body.classList.add(activeClass)
+      } else {
+        document.body.classList.remove(activeClass)
+      }
+    }
+    document.querySelector('.' + toggleClass).addEventListener('click', toggle)
+  }
+}
+
+toggleNav('nav-toggle', 'nav-active')
+
 
 // Modals
 var candidates = document.querySelectorAll('#candidates')
